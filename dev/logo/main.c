d767f3141613534dc794e6679917210e1edf048b
pascalmaxime <mpascal42@icloud.com>
2025-02-17 10:41:57 +0100

    v°1.13 17/02/2025

--- dev/logo/main.c
+++ dev/logo/main.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Grille de 20 * 20
+#define LARGEUR 20
+#define HAUTEUR 20
+
+int grille[HAUTEUR][LARGEUR];
+
 int main ()
 {
-    printf("hello logo");
+    printf("hello logo\n");
     return EXIT_SUCCESS;
 
 }
