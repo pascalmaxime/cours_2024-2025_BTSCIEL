ae2abbc8d8e4bb2c2fd5560d35269c0cd4250c88
pascalmaxime <mpascal42@icloud.com>
2025-02-17 10:57:16 +0100

    v°1.14 17/02/2025

--- dev/logo/main.c
+++ dev/logo/main.c
@@ -5,11 +5,24 @@
 #define LARGEUR 20
 #define HAUTEUR 20
 
+typedef struct position_t {
+    int x;
+    int y;
+} Position;
+
 int grille[HAUTEUR][LARGEUR];
 
 int main ()
 {
+    Position joueur = {0, 0};
+    Position cible;
+
     printf("hello logo\n");
+    cible.x = 5;
+    cible.y = 5;
+
+    printf("Joueur: x=%d y=%d", joueur.x, joueur.y);
+    printf("Cible: x=%d y=%d", cible.x, cible.y);
     return EXIT_SUCCESS;
 
 }
