6f7fa8dfc5f7d89344c09cc8a00b8752f8b9224a
pascalmaxime <mpascal42@icloud.com>
2025-02-19 11:10:05 +0100

    19/02/25 11h10

--- dev/Exercices/Algo/paire_impaire.c
+++ dev/Exercices/Algo/paire_impaire.c
@@ -1,18 +1,26 @@
 #include <stdio.h>
 
-int main(void)
+int pair_impair(int nbr);
 
+int main()
 {
    int nbr;
    
    printf("Entrez un entier:");
    scanf("%d", &nbr);
    
-   if (nbr % 2 == 0)
+   if (pair_impair(nbr) == 0)
       printf("Paire\n");
    else
       printf("Impaire\n");
-     
    return 0;
 }
 
+int pair_impair(int nbr){
+    if (nbr % 2 == 0){
+        return 0;
+    }
+    else{
+        return 1;
+    }
+}
