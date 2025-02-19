551f88c58d1829d1c83e2e34dfc9e07cc9ccab15
pascalmaxime <mpascal42@icloud.com>
2025-02-19 10:57:12 +0100

    19/02/25 10h57

--- dev/Exercices/Algo/paire_impaire.c
+++ dev/Exercices/Algo/paire_impaire.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
- 
-int main()
+
+int main(void)
+
 {
    int nbr;
    
@@ -15,20 +16,3 @@ int main()
    return 0;
 }
 
-
-/*
-#include <stdio.h>
-#include <stdlib.h>
-
-
-int main() {
-    int nombre;
-
-    printf("Entrez un nombre : ");
-    scanf("%d", &nombre); 
-
-    nombre /= 2;
-    printf("La valeur absolue de %d \n", nombre);    
-    
-    return 0;
-}*/
